0efae29560099d9f3c9355f09a1eb545c36aef07
hemant12797 <hemantthakur8439@gmail.com>
2026-05-16 17:23:37 +0530

    Update fronted API URLs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,19 @@
 export const environment = {
   production: false,
-  googleClientId: '994487737899-ut3f47kerb5so3p14q5pddr7p0iqb6uh.apps.googleusercontent.com', // Updated from backend
-  authApi: 'http://localhost:5290/api/users',
-  expenseApi: 'http://localhost:5025/api/expenses',
-  budgetApi: 'http://localhost:5149/api/budgets',
-  categoryApi: 'http://localhost:5254/api/categories',
-  notificationApi: 'http://localhost:5207/notificationHub',
-  reportApi: 'http://localhost:5220/api/reports',
-  incomeApi: 'http://localhost:5252/api/incomes'
+
+  googleClientId: '994487737899-ut3f47kerb5so3p14q5pddr7p0iqb6uh.apps.googleusercontent.com',
+
+  authApi: 'https://auth-api.redpond-13ac99d0.centralindia.azurecontainerapps.io/api/users',
+
+  expenseApi: 'https://expense-api.redpond-13ac99d0.centralindia.azurecontainerapps.io/api/expenses',
+
+  budgetApi: 'https://budget-api.redpond-13ac99d0.centralindia.azurecontainerapps.io/api/budgets',
+
+  categoryApi: 'https://category-api.redpond-13ac99d0.centralindia.azurecontainerapps.io/api/categories',
+
+  notificationApi: 'https://notification-api.redpond-13ac99d0.centralindia.azurecontainerapps.io/notificationHub',
+
+  reportApi: 'https://report-api.redpond-13ac99d0.centralindia.azurecontainerapps.io/api/reports',
+
+  incomeApi: 'https://income-api.redpond-13ac99d0.centralindia.azurecontainerapps.io/api/incomes'
 };
